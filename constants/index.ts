baa00ad770ee35fb0c8eb2fee72d22e25f5269a0
theRunCom <1772414249@qq.com>
2024-02-04 12:52:28 +0800

    🔔Update utils

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const Socials = [
+  {
+    name: "X",
+    src: "/twitter.svg",
+    link: "https://twitter.com/EquatorFOCG",
+  },
+  {
+    name: "Github",
+    src: "/github.svg",
+    link: "https://github.com/Equator-FOCG",
+  },
+  {
+    name: "Discord",
+    src: "/discord.svg",
+    link: "https://discord.gg/neF447HhcF",
+  },
+];
