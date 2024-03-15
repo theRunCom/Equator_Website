9acf5403412c7eed7510a616d9d9208a725c6e6a
theRunCom <1772414249@qq.com>
2024-03-15 21:51:45 +0800

    Fix discord link

--- constants/index.ts
+++ constants/index.ts
@@ -5,13 +5,13 @@ export const Socials = [
     link: "https://twitter.com/EquatorFOCG",
   },
   {
+    name: "Discord",
+    src: "/discord.svg",
+    link: "https://discord.gg/68uQz9DXWW",
+  },
+  {
     name: "Github",
     src: "/github.svg",
     link: "https://github.com/Equator-FOCG",
-  },
-  {
-    name: "Discord",
-    src: "/discord.svg",
-    link: "https://discord.gg/neF447HhcF",
   },
 ];
