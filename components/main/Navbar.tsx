ddfd0e275ca2294c5c7268c1eab7812b7d6aaa01
theRunCom <1772414249@qq.com>
2024-03-15 22:02:13 +0800

    Add theme switch button

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -5,16 +5,16 @@ import {
   HomeIcon,
   FingerPrintIcon,
   DocumentMagnifyingGlassIcon,
+  CreditCardIcon,
+  GlobeAltIcon,
 } from "@heroicons/react/24/solid";
+import { ThemedButton } from "./ThemedButton";
 
 const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a
-          href="#home"
-          className="h-auto w-auto flex flex-row items-center"
-        >
+        <a href="#" className="h-auto w-auto flex flex-row items-center">
           <Image
             src="/FOCG_Logo.png"
             alt="logo"
@@ -30,14 +30,20 @@ const Navbar = () => {
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#home" className="cursor-pointer">
-              <HomeIcon className="text-[#9e56d8] ml-[20px] h-8 w-8" />
+            <a href="#projects" className="cursor-pointer">
+              <DocumentMagnifyingGlassIcon className="text-[#9e56d8] mr-[20px] h-8 w-8" />
             </a>
             <a href="#testimonials" className="cursor-pointer">
               <FingerPrintIcon className="text-[#9e56d8] h-8 w-8" />
             </a>
-            <a href="#projects" className="cursor-pointer">
-              <DocumentMagnifyingGlassIcon className="text-[#9e56d8] mr-[20px] h-8 w-8" />
+            <a href="#home" className="cursor-pointer">
+              <HomeIcon className="text-[#9e56d8] ml-[20px] h-8 w-8" />
+            </a>
+            <a href="#" className="cursor-pointer">
+              <GlobeAltIcon className="text-[#9e56d8] ml-[20px] h-8 w-8" />
+            </a>
+            <a href="#" className="cursor-pointer">
+              <CreditCardIcon className="text-[#9e56d8] ml-[20px] h-8 w-8" />
             </a>
           </div>
         </div>
@@ -54,6 +60,8 @@ const Navbar = () => {
               />
             </a>
           ))}
+          <div style={{ borderRight: "1px dashed #999", height: "30px" }}></div>
+          <ThemedButton />
         </div>
       </div>
     </div>
