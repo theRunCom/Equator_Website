b69441f4ad9198cf7e3efa6aab175303096839ed
theRunCom <1772414249@qq.com>
2024-03-16 16:07:27 +0800

    Update Footer

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,70 +1,87 @@
 import React from "react";
-import {
-  RxDiscordLogo,
-  RxGithubLogo,
-  RxInstagramLogo,
-  RxTwitterLogo,
-  RxLinkedinLogo,
-} from "react-icons/rx";
-
-import { FaYoutube } from "react-icons/fa";
+import { RxDiscordLogo, RxGithubLogo, RxTwitterLogo } from "react-icons/rx";
+import { IoDocumentTextSharp, IoGameController } from "react-icons/io5";
+import { ImBlog } from "react-icons/im";
+import { CiMail } from "react-icons/ci";
+import { SiGithubsponsors } from "react-icons/si";
+import { MdOutlineEmojiObjects } from "react-icons/md";
 
 const Footer = () => {
-  return (
-    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
-      <div className="w-full flex flex-col items-center justify-center m-auto">
-        <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
-          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-            <div className="font-bold text-[16px]">Community</div>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <FaYoutube />
-              <span className="text-[15px] ml-[6px]">Youtube</span>
-            </p>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <RxGithubLogo />
-              <span className="text-[15px] ml-[6px]">Github</span>
-            </p>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <RxDiscordLogo />
-              <span className="text-[15px] ml-[6px]">Discord</span>
-            </p>
-          </div>
-          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-            <div className="font-bold text-[16px]">Social Media</div>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <FaYoutube />
-              <span className="text-[15px] ml-[6px]">Instagram</span>
-            </p>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <RxGithubLogo />
-              <span className="text-[15px] ml-[6px]">Twitter</span>
-            </p>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <RxDiscordLogo />
-              <span className="text-[15px] ml-[6px]">Linkedin</span>
-            </p>
-          </div>
-          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-            <div className="font-bold text-[16px]">About</div>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <span className="text-[15px] ml-[6px]">Become Sponsor</span>
-            </p>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <span className="text-[15px] ml-[6px]">Learning about me</span>
-            </p>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <span className="text-[15px] ml-[6px]">
-                mifwebchain@gmail.com
-              </span>
-            </p>
-          </div>
-        </div>
+  const d = new Date();
+  const currentYear = d.getFullYear();
 
-        <div className="mb-[20px] text-[15px] text-center">
-          &copy; Equator 2024 Inc. All rights reserved
+  return (
+    <footer className="absolute w-full">
+      <div className="bg-transparent shadow-lg p-[15px] text-theme">
+        <div className="w-full flex flex-col items-center justify-center m-auto">
+          <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
+            <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+              <div className="font-bold text-[16px]">Community</div>
+              <a
+                href="https://twitter.com/EquatorFOCG"
+                target="_blank"
+                className="flex flex-row items-center my-[15px] cursor-pointer"
+              >
+                <RxTwitterLogo />
+                <span className="text-[15px] ml-[6px]">X</span>
+              </a>
+              <a
+                href="https://github.com/Equator-FOCG"
+                target="_blank"
+                className="flex flex-row items-center my-[15px] cursor-pointer"
+              >
+                <RxGithubLogo />
+                <span className="text-[15px] ml-[6px]">Github</span>
+              </a>
+              <a
+                href="https://discord.gg/68uQz9DXWW"
+                target="_blank"
+                className="flex flex-row items-center my-[15px] cursor-pointer"
+              >
+                <RxDiscordLogo />
+                <span className="text-[15px] ml-[6px]">Discord</span>
+              </a>
+            </div>
+            <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+              <div className="font-bold text-[16px]">Quick Links</div>
+              <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                <IoDocumentTextSharp />
+                <span className="text-[15px] ml-[6px]">Tutorials</span>
+              </p>
+              <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                <ImBlog />
+                <span className="text-[15px] ml-[6px]">Blogs</span>
+              </p>
+              <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                <IoGameController />
+                <span className="text-[15px] ml-[6px]">Games</span>
+              </p>
+            </div>
+            <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+              <div className="font-bold text-[16px]">About</div>
+              <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                <SiGithubsponsors />
+                <span className="text-[15px] ml-[6px]">Support</span>
+              </p>
+              <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                <MdOutlineEmojiObjects />
+                <span className="text-[15px] ml-[6px]">Introduction</span>
+              </p>
+              <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                <CiMail />
+                <span className="text-[15px] ml-[6px]">
+                  theRunCom@protonmail.com
+                </span>
+              </p>
+            </div>
+          </div>
+
+          <div className="mb-[20px] text-[15px] text-center">
+            &copy; Equator {currentYear} Inc. All rights reserved
+          </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
